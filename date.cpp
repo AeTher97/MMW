13f3f1dfe2a6db7144c2983b90c8b82c981031ed
Wojciech Tomasiewicz <wojtarek@vp.pl>
2017-10-25 08:57:51 +0200

    -a

--- date.cpp
+++ date.cpp
@@ -7,6 +7,20 @@ Date::Date(int _year, int _month, int _day)
 	day=_day;
 }
 
+void Date::checkdate()
+{
+	while(day>days_in_months[month-1])
+	{
+		day -= days_in_months[month - 1];
+		month++;
+	}
+	while(month>12)
+	{
+		month -= 12;
+		year++;
+	}
+}
+
 Date::Date& operator+(int numberOfDays) const
 {
 
