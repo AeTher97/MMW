13fd37873de923d7fd43b82923e60d3c1c0397d0
AeTher97 <michael93509@gmail.com>
2017-10-25 08:47:01 +0200

    Create date.cpp

---
+++ date.cpp
@@ -0,0 +1,17 @@
+#include "date.hpp"
+
+Date::Date(int _year, int _month, int _day)
+{
+	year=_year;
+	month=_month;
+	day=_day;
+}
+
+Date::Date& operator+(int numberOfDays) const
+{
+
+
+	return 0;
+}
+
+
